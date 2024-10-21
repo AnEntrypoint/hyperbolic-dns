1a0906552180e9419da07f89ccfe6dc6f9706142
lanmower <almagestfraternite@gmail.com>
2024-10-21 16:23:38 +0200

    Update dns.js

--- dns.js
+++ dns.js
@@ -1,3 +1,5 @@
+// DNS server
+
 const DHT = require("hyperdht");
 const node = new DHT();
 require("dotenv").config();
@@ -9,14 +11,17 @@ const fs = require("fs");
 const known = {};
 const NodeCache = require("node-cache");
 const cache = new NodeCache();
-const { lookup } = require('hyper-ipc-secure')()
+const { lookup } = require('hyper-ipc-secure')();
 const reverseDomain = process.env.reverseDomain;
 const namespace = process.env.hypernamespace;
+
 const handle = async (request, send) => {
     const response = Packet.createResponseFromRequest(request);
     const [question] = request.questions;
-    console.log('question', {question});
-    if(question.name.toLowerCase().startsWith('mail')) {
+    console.log('question', { question });
+
+    // Handle mail A records
+    if (question.name.toLowerCase().startsWith('mail')) {
         const ip = '150.136.80.119';
         response.answers = [
             {
@@ -26,53 +31,54 @@ const handle = async (request, send) => {
                 class: Packet.CLASS.IN,
                 ttl: 3600,
             },
-          ];        
-          send(response);
-          //console.log(response)
-          return; 
-    }
-    if(question.type == 15) {
-        console.log("MX RECORD")
-        response.answers = [
-            {
-              type: Packet.TYPE.MX,
-              name: question.name.toLowerCase(),
-              exchange:'mail.247420.xyz',
-              priority:'10',
-              class: Packet.CLASS.IN,
-              ttl: 1,
-            },
-          ];
-        send(response);
-        //console.log(response)
-        return;        
-    }
-    if (question.type == 16) {
-        //console.log({ question });
-        response.answers = [
-            {
-              type: Packet.TYPE.TXT,
-              name: question.name.toLowerCase(),
-              data:'google-site-verification=PguQv_YbBtFq8QCtyzH-z95Tqh9B_gIJevdriRe9GQ8',
-              class: Packet.CLASS.IN,
-              ttl: 1,
-            },
-          ];
+        ];
         send(response);
         return;
-      }
-      
+    }
+
+    // Handle MX records
+    if (question.type == 15) {
+        console.log("MX RECORD");
+        response.answers = [
+            {
+                type: Packet.TYPE.MX,
+                name: question.name.toLowerCase(),
+                exchange: 'mail.247420.xyz',
+                priority: '10',
+                class: Packet.CLASS.IN,
+                ttl: 1,
+            },
+        ];
+        send(response);
+        return;
+    }
+
+    // Handle TXT records
+    if (question.type == 16) {
+        response.answers = [
+            {
+                type: Packet.TYPE.TXT,
+                name: question.name.toLowerCase(),
+                data: 'google-site-verification=PguQv_YbBtFq8QCtyzH-z95Tqh9B_gIJevdriRe9GQ8',
+                class: Packet.CLASS.IN,
+                ttl: 1,
+            },
+        ];
+        send(response);
+        return;
+    }
+
     let { name } = question;
     name = name.toLowerCase();
     let split = name.split(".");
     if (!split[split.length - 3]) {
         split = ['', split[0], split[1]];
     }
-    
+
     const outname = namespace + split[split.length - 3];
-    console.log({outname});
+    console.log({ outname });
+
     if (name.endsWith('.in-addr.arpa')) {
-        const response = Packet.createResponseFromRequest(request);
         response.answers = [
             {
                 type: Packet.TYPE.PTR,
@@ -80,11 +86,12 @@ const handle = async (request, send) => {
                 domain: reverseDomain,
                 class: Packet.CLASS.IN,
                 ttl: 3600,
-            }
+            },
         ];
         send(response);
         return;
     }
+
     const cached = cache.get(namespace + split.join('.'));
     if (cached) {
         response.answers = cached.data.answers;
@@ -94,85 +101,59 @@ const handle = async (request, send) => {
         return send(response);
     }
 
-    if (!split) {
-        return send(response);
-    }
-
     let decoded = '';
     let result;
-    try { decoded = b32.decode.asBytes(name.toUpperCase()) } catch (e) { }
+    try { decoded = b32.decode.asBytes(name.toUpperCase()); } catch (e) { }
     if (decoded.length == 32) publicKey = Buffer.from(decoded);
     else {
         if (!known[name]) {
             try {
                 known[name] = JSON.parse(fs.readFileSync('known/' + name));
                 known[name].key = Buffer.from(known[name].keyback, 'hex');
-            } catch (e) {
-            }
-        }
-        if (known[name] && new Date().getTime() - known[name].last < 15 * 60 * 1000) {
-            console.log('LOOKING UP', namespace)
-            result = await lookup(namespace);
-        } else {
-            console.log('LOOKING UP', namespace)
-            result = await lookup(namespace);
-        }
-        async function toArray(iterable) {
-            console.log(iterable);
-            const result = []
-            for await (const data of iterable) result.push(data)
-            return result
+            } catch (e) { }
         }
 
-        const connectAndGetIp = (target) => {
-            return new Promise(res => {
-                let socket = node.connect(target);
-                socket.write('dns');
-                socket.once("error", function (data) {
-                    res();
-                });
+        const peersStream = node.lookup(Buffer.from(outname), {});
+        peersStream.on('data', async (peerInfo) => {
+            const { peers } = peerInfo;
 
-                socket.once("data", function (data) {
-                    if (!response.authorities.length) {
-                        response.header.aa = 1;
-                    }
-                    res(JSON.parse(data)?.host);
-                });
-
-            })
-        }
-
-        console.log({result})
-        if (result.length > 0) {
-            let ip;
-            for (res of result) {
-                for (peer of res.peers) {
-                    if (!known[name]) known[name] = {};
-                    known[name].ip = ip = await connectAndGetIp(peer.publicKey);
-                    console.log({ip})
-                    if (ip) {
-                        known[name] = {};
-                        known[name].last = new Date().getTime();
-                        known[name].key = peer.publicKey;
-                        known[name].keyback = known[name]?.key?.toString('hex');
-                        fs.writeFileSync('known/' + name, JSON.stringify(known[name]));
-                        response.answers.push({
-                            type: Packet.TYPE.A,
-                            name: question.name.toLowerCase(),
-                            address: ip,
-                            class: Packet.CLASS.IN,
-                            ttl: 3600,
-                        });
-                        cache.set('hyperbolic' + split.join('.'), { data: response, time: new Date().getTime() }, 300000);
-                        return send(response);
-                    }
+            for (const peer of peers) {
+                const ip = await connectAndGetIp(peer.publicKey);
+                if (ip) {
+                    response.answers.push({
+                        type: Packet.TYPE.A,
+                        name: question.name.toLowerCase(),
+                        address: ip,
+                        class: Packet.CLASS.IN,
+                        ttl: 3600,
+                    });
+                    known[name] = {
+                        last: new Date().getTime(),
+                        key: peer.publicKey,
+                        keyback: peer.publicKey.toString('hex'),
+                    };
+                    fs.writeFileSync('known/' + name, JSON.stringify(known[name]));
+                    cache.set(namespace + split.join('.'), { data: response, time: new Date().getTime() }, 300000);
+                    return send(response);
                 }
             }
-            if (known[name]) target = known[name].key;
-        }
+        });
     }
+};
 
-    return send(response);
+// Helper function to connect to a peer and get their IP address
+const connectAndGetIp = async (targetPublicKey) => {
+    return new Promise((resolve) => {
+        const socket = node.connect(targetPublicKey);
+        socket.write('dns');
+        socket.once("error", function () {
+            resolve(null); // If there's an error, return null
+        });
+
+        socket.once("data", function (data) {
+            resolve(JSON.parse(data)?.host); // Return the host found
+        });
+    });
 };
 
 const server = dns2.createServer({
